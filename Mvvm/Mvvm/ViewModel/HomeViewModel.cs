65e9cf99be09abff465b63e921c7f97361c8c059
venkat kiran kumar garikipati <gvkiran.garikipati@outlook.com>
2019-06-18 10:37:38 +0530

    Add project files.

---
+++ Mvvm/Mvvm/ViewModel/HomeViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Mvvm.ViewModel
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return $"enter name is {Description}";
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
